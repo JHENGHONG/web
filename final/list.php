addc0e963170d461e0caeaaf382c21073aff0b60
JHENGHONG <s01224566@gmail.com>
2018-06-09 23:50:11 +0800

    期末作業

---
+++ final/list.php
@@ -0,0 +1,33 @@
+<?php
+
+try {
+	$db = new PDO('mysql:host=localhost;dbname=mid;charset=utf8'
+		,'d04220208','123456', array( 
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+		) );
+}catch(PDOException $err) {
+	http_response_code(500);
+	echo 'failed';
+	exit;
+}
+
+$stmt = $db->prepare('select * from messageboard');
+$stmt->execute();
+
+$data = array();
+
+while($row = $stmt->fetch()){  
+	$data[] = (object)[
+			'tt' => $row['title'],
+			'nn' => $row['name'],
+			'mm' => $row['message'],
+			'id' => $row['m_id']
+		];
+}
+
+
+
+http_response_code(200);
+header("Content-Type: application/json;charset=UTF-8");
+echo json_encode($data);            
